511a95a3e98fdfc0fa38cbceb254cea2adb7e54f
mishi <126573469+mishisz@users.noreply.github.com>
2023-05-29 18:05:18 -0300

    Create algoritmo16.go

---
+++ algoritmo16.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str1, str2 string
+	fmt.Print("Digite a primeira string: ")
+	fmt.Scanln(&str1)
+	fmt.Print("Digite a segunda string: ")
+	fmt.Scanln(&str2)
+
+	isSubstring := strings.Contains(str1, str2)
+
+	if isSubstring {
+		fmt.Println("A segunda string é uma substring da primeira.")
+	} else {
+		fmt.Println("A segunda string não é uma substring da primeira.")
+	}
+}
